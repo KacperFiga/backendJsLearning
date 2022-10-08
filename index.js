0724d84569a2ed7062043031accc121d9215a9ac
KacperFiga <kacper.figa.com@o2.pl>
2022-10-08 16:24:43 +0200

    add simple routes

--- index.js
+++ index.js
@@ -8,6 +8,36 @@ app.get("/", (req, res) => {
   res.send("<h1>Hello world</h1>");
 });
 
+app.get("/api/v1/instagram", (req, res) => {
+  const data = {
+    username: "Figo",
+    followers: 2137,
+    follows: 3127,
+    date: Date.now(),
+  };
+  res.status(200).json(data);
+});
+
+app.get("/api/v1/facebook", (req, res) => {
+  const data = {
+    username: "Figo",
+    followers: 2137,
+    follows: 3127,
+    date: Date.now(),
+  };
+  res.status(200).json(data);
+});
+
+app.get("/api/v1/linkedin", (req, res) => {
+  const data = {
+    username: "Figo",
+    followers: 2137,
+    follows: 3127,
+    date: Date.now(),
+  };
+  res.status(200).json(data);
+});
+
 app.listen(PORT, () => {
   console.log(`Server is running at port ${PORT}`);
 });
